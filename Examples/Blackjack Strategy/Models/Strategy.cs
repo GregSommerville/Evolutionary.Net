a5d6e85ce474dae5e7d9cd1b6e80c08856ecf2ed
gregsommerville <me@gregsommerville.com>
2019-01-01 16:22:06 -0500

    Finished class for strategy storage

---
+++ Examples/Blackjack Strategy/Models/Strategy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackjackStrategy.Models
+{
+    // encapsulates one complete strategy to play Blackjack
+    class OverallStrategy
+    {
+        // how to play pairs, key: 77 A (meaning, a pair of 7s with a dealer upcard of an ace)
+        private Dictionary<string, ActionToTake> pairsStrategy = new Dictionary<string, ActionToTake>();
+
+        // how to play soft hands, key: A33 6 (Ace-three-three in hand, with dealer upcard of six)
+        private Dictionary<string, ActionToTake> softStrategy = new Dictionary<string, ActionToTake>();
+
+        // how to play hard hands, keyed by the hand total and upcard: 17 3
+        private Dictionary<string, ActionToTake> hardStrategy = new Dictionary<string, ActionToTake>();
+
+        //-------------------------------------------------------------------------------------------
+
+        public void AddPairStrategy(string pairRank, string dealerUpcard, ActionToTake action)
+        {
+            pairsStrategy[pairRank + " " + dealerUpcard] = action;
+        }
+        public void AddSoftStrategy(string hand, string dealerUpcard, ActionToTake action)
+        {
+            softStrategy[hand + " " + dealerUpcard] = action;
+        }
+        public void AddHardStrategy(int handTotal, string dealerUpcard, ActionToTake action)
+        {
+            hardStrategy[handTotal + " " + dealerUpcard] = action;
+        }
+
+        //-------------------------------------------------------------------------------------------
+
+        public ActionToTake GetActionForPair(string pairRank, string dealerUpcard)
+        {
+            return pairsStrategy[pairRank + " " + dealerUpcard];
+        }
+        public ActionToTake GetActionForSoftHand(string hand, string dealerUpcard)
+        {
+            return softStrategy[hand + " " + dealerUpcard];
+        }
+        public ActionToTake GetActionForHardHand(int handTotal, string dealerUpcard)
+        {
+            return hardStrategy[handTotal + " " + dealerUpcard];
+        }
+    }
+}
